6488dd83e98249c747c502e437d2a0b93a22eb6a
Atu77l <kesharwaniatul9935@gmail.com>
2023-12-26 21:32:45 +0530

    updated

--- src/components/Achievement/Achievement.js
+++ src/components/Achievement/Achievement.js
@@ -8,11 +8,11 @@ function Achieve() {
     <div>
       <h2 className='section__title font-semibold mt-10 mb-10' id='achieve'>Achievement</h2>
       <ol className="list">
-      <li className="item"><CheckBoxIcon sx={{color:"blue"}}/><span className="sp1 ml-2">541th</span> rank in Codekaze 2022 out of 1 lakh participant.</li>
-      <li className="item"><CheckBoxIcon sx={{color:"blue"}}/><span className="sp1 ml-2">571th</span> rank in Codekaze 2021 out of 25000 partcipant.</li>
-      <li className="item"><CheckBoxIcon sx={{color:"blue"}}/><span className="sp1 ml-2">3 star</span> rating in Codechef.<span className="sp1"><a href="https://www.codechef.com/users/atulkesharwani">link</a></span></li>
-      <li className="item"><CheckBoxIcon sx={{color:"blue"}}/>  I have solved <span className="sp1">900</span> question in leetcode.<a href="https://leetcode.com/atulkesharwani/"><span className="sp1">link</span></a></li>
-      <li className="item"><CheckBoxIcon sx={{color:"blue"}}/><span className="sp1 ml-2">5star</span> rating in hackerrank.<a href="https://www.hackerrank.com/kesharwaniatul99"><span className="sp1">link</span></a></li>
+      <li className="item"><CheckBoxIcon style={{color:"var(--clr-primary)"}}/><span className="sp1 ml-2">541th</span> rank in Codekaze 2022 out of 1 lakh participant.</li>
+      <li className="item"><CheckBoxIcon style={{color:"var(--clr-primary)"}}/><span className="sp1 ml-2">571th</span> rank in Codekaze 2021 out of 25000 partcipant.</li>
+      <li className="item"><CheckBoxIcon style={{color:"var(--clr-primary)"}}/><span className="sp1 ml-2">3 star</span> rating in Codechef.<span className="sp1"><a href="https://www.codechef.com/users/atulkesharwani">link</a></span></li>
+      <li className="item"><CheckBoxIcon style={{color:"var(--clr-primary)"}}/>  I have solved <span className="sp1">900</span> question in leetcode.<a href="https://leetcode.com/atulkesharwani/"><span className="sp1">link</span></a></li>
+      <li className="item"><CheckBoxIcon style={{color:"var(--clr-primary)"}}/><span className="sp1 ml-2">5star</span> rating in hackerrank.<a href="https://www.hackerrank.com/kesharwaniatul99"><span className="sp1">link</span></a></li>
       </ol>
     </div>
   )
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,9 @@
 import './Footer.css'
+import GitHubIcon from '@material-ui/icons/GitHub'
+import LinkedInIcon from '@material-ui/icons/LinkedIn'
+import TwitterIcon from '@material-ui/icons/Twitter';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import CopyrightIcon from '@mui/icons-material/Copyright';
 
 const Footer = () => (
   <footer className='footer'>
@@ -7,8 +12,10 @@ const Footer = () => (
       className='link footer__link'
     >
       <h3 className='t'>Created By : </h3>
-      <h3 className='g'>Akash kesharwani</h3> 
+      <h3 className='text-white'>Akash kesharwani</h3> 
     </a>
+    <h6 className='mt-5 text-white'><CopyrightIcon/>Copyright 2023 Akash Kesharwani. All Rights Reserved.</h6>
+    <div className='text-white'><GitHubIcon/><LinkedInIcon/><TwitterIcon/><InstagramIcon/></div>
   </footer>
 )
 
